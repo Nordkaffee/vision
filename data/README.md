e8e273b908670742471f1e4c03f10f1cb4d85a6d
philippmwirth <philipp.m.wirth@gmail.com>
2021-03-20 18:05:54 +0100

    Clean up and update readme

---
+++ data/README.md
@@ -0,0 +1,14 @@
+# Image Download
+
+Helper to scrape DuckDuckGo for images of interest.
+
+## Installation
+TODO
+
+## Example
+
+Scan DuckDuckGo for 100 images of "Coffea Arabica" and store images in `datasets/`.
+
+```python
+python3 scrape.py --target_dir 'datasets' --query 'Coffea Arabica' --max_results 100
+```
--- data/scrape.py
+++ data/scrape.py
@@ -1,11 +1,24 @@
-from jmd_imagescraper.core import *
+""" Script to scrape images from DuckDuckGo """
+
 import os
+import argparse
+from jmd_imagescraper.core import *
 
-# config
+# parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--target_dir', type=str, default='datasets/scraped/',
+                    help='Relative path to where images will be stored.')
+parser.add_argument('--query', type=str, default='Coffea Arabica',
+                    help='Query to search for in DuckDuckGo.')
+parser.add_argument('--max_results', type=int, default=1000,
+                    help='Maximum number of images to scrape.')
+args = parser.parse_args()
+
+# query images from duck duck go
 root_dir = os.getcwd()
-target_dir = "datasets/CoffeaArabica_4"
-query = "Coffea Arabica"
-max_results = 1000
-
-# query
-duckduckgo_search(root_dir, target_dir, query, max_results=max_results)
+duckduckgo_search(
+    root_dir,
+    args.target_dir,
+    args.query,
+    max_results=args.max_results
+)
